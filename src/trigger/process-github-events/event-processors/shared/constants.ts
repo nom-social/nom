2ae4cdb746c767cd5b84d20b35e3de612bc9702c
lws803 <lws803@gmail.com>
2025-06-15 08:21:26 -0400

    bumped up pull request review multiplier

--- src/trigger/process-github-events/event-processors/shared/constants.ts
+++ src/trigger/process-github-events/event-processors/shared/constants.ts
@@ -2,5 +2,5 @@ export const BASELINE_SCORE = 1;
 
 export const ISSUE_MULTIPLIER = 1;
 export const PULL_REQUEST_MULTIPLIER = 2;
-export const PULL_REQUEST_REVIEW_MULTIPLIER = 2;
+export const PULL_REQUEST_REVIEW_MULTIPLIER = 3;
 export const RELEASE_MULTIPLIER = 1;
