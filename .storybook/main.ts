740b87162df439a045c631c104bcaf05eb6b130a
Wilson Ler <lws803@gmail.com>
2025-06-26 09:45:09 -0400

    removed docs addon for now

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -5,7 +5,6 @@ const config: StorybookConfig = {
   stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
   addons: [
     "@chromatic-com/storybook",
-    "@storybook/addon-docs",
     "@storybook/addon-onboarding",
     "@storybook/addon-a11y",
     "@storybook/addon-vitest",
