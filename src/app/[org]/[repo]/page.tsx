8189b46790a5d0d630871b2e321129d100dfddf4
Wilson Ler <lws803@gmail.com>
2025-06-28 11:02:47 -0400

    updated protected pages

---
+++ src/app/[org]/[repo]/page.tsx
@@ -0,0 +1,22 @@
+"use client";
+
+import { useParams } from "next/navigation";
+
+export default function RepoPage() {
+  const params = useParams();
+  const { org, repo } = params as { org: string; repo: string };
+
+  return (
+    <div className="min-h-screen flex items-center justify-center">
+      <div className="text-center">
+        <h1 className="text-3xl font-bold mb-2">Repo Page</h1>
+        <p className="text-xl">
+          Org: <span className="font-mono">{org}</span>
+        </p>
+        <p className="text-xl">
+          Repo: <span className="font-mono">{repo}</span>
+        </p>
+      </div>
+    </div>
+  );
+}
