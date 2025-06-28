54c1ebe8be95ee307717b7fddecc51dfbc7cc161
Wilson Ler <lws803@gmail.com>
2025-06-28 01:25:43 -0400

    [NOM-85] Make nom project open source ready (#51)
    * refactored code
    
    * added migrations
    
    * added license
    
    * added sample issue template
    
    * added sample .env.sample

---
+++ .github/PULL_REQUEST_TEMPLATE.md
@@ -0,0 +1,18 @@
+# Pull Request Template
+
+## Description
+
+<!-- Please include a summary of the change and which issue is fixed. Also include relevant motivation and context. -->
+
+Fixes # (issue)
+
+## Type of change
+
+- [ ] Bug fix
+- [ ] New feature
+- [ ] Breaking change
+- [ ] Documentation update
+
+## Additional context
+
+<!-- Add any other context or screenshots about the pull request here. -->
---
+++ supabase/migrations/20250628050723_remote_schema.sql
@@ -0,0 +1,541 @@
+
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET xmloption = content;
+SET client_min_messages = warning;
+SET row_security = off;
+
+
+COMMENT ON SCHEMA "public" IS 'standard public schema';
+
+
+
+CREATE EXTENSION IF NOT EXISTS "pg_graphql" WITH SCHEMA "graphql";
+
+
+
+
+
+
+CREATE EXTENSION IF NOT EXISTS "pg_stat_statements" WITH SCHEMA "extensions";
+
+
+
+
+
+
+CREATE EXTENSION IF NOT EXISTS "pgcrypto" WITH SCHEMA "extensions";
+
+
+
+
+
+
+CREATE EXTENSION IF NOT EXISTS "supabase_vault" WITH SCHEMA "vault";
+
+
+
+
+
+
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp" WITH SCHEMA "extensions";
+
+
+
+
+
+SET default_tablespace = '';
+
+SET default_table_access_method = "heap";
+
+
+CREATE TABLE IF NOT EXISTS "public"."github_event_log" (
+    "id" "uuid" DEFAULT "gen_random_uuid"() NOT NULL,
+    "created_at" timestamp with time zone DEFAULT ("now"() AT TIME ZONE 'utc'::"text") NOT NULL,
+    "event_type" "text" NOT NULL,
+    "action" "text",
+    "repo" "text" NOT NULL,
+    "org" "text" NOT NULL,
+    "raw_payload" "jsonb" NOT NULL,
+    "last_processed" timestamp with time zone
+);
+
+
+ALTER TABLE "public"."github_event_log" OWNER TO "postgres";
+
+
+CREATE TABLE IF NOT EXISTS "public"."public_timeline" (
+    "id" "uuid" DEFAULT "gen_random_uuid"() NOT NULL,
+    "created_at" timestamp with time zone DEFAULT ("now"() AT TIME ZONE 'utc'::"text") NOT NULL,
+    "score" integer NOT NULL,
+    "is_read" boolean DEFAULT false NOT NULL,
+    "data" "jsonb" NOT NULL,
+    "repo_id" "uuid",
+    "type" "text" NOT NULL,
+    "categories" "text"[],
+    "snooze_to" timestamp with time zone,
+    "dedupe_hash" "text" NOT NULL,
+    "updated_at" timestamp with time zone DEFAULT ("now"() AT TIME ZONE 'utc'::"text") NOT NULL,
+    "event_ids" "text"[]
+);
+
+
+ALTER TABLE "public"."public_timeline" OWNER TO "postgres";
+
+
+COMMENT ON TABLE "public"."public_timeline" IS 'This is a duplicate of user_timeline';
+
+
+
+COMMENT ON COLUMN "public"."public_timeline"."event_ids" IS 'Mostly for backtracking and debugging purposes';
+
+
+
+CREATE TABLE IF NOT EXISTS "public"."repositories" (
+    "id" "uuid" DEFAULT "gen_random_uuid"() NOT NULL,
+    "created_at" timestamp with time zone DEFAULT ("now"() AT TIME ZONE 'utc'::"text") NOT NULL,
+    "repo" "text" NOT NULL,
+    "org" "text" NOT NULL,
+    "access_token" "text",
+    "secret" "text"
+);
+
+
+ALTER TABLE "public"."repositories" OWNER TO "postgres";
+
+
+CREATE TABLE IF NOT EXISTS "public"."subscriptions" (
+    "id" "uuid" DEFAULT "gen_random_uuid"() NOT NULL,
+    "created_at" timestamp with time zone DEFAULT ("now"() AT TIME ZONE 'utc'::"text") NOT NULL,
+    "user_id" "uuid" NOT NULL,
+    "repo_id" "uuid" NOT NULL
+);
+
+
+ALTER TABLE "public"."subscriptions" OWNER TO "postgres";
+
+
+CREATE TABLE IF NOT EXISTS "public"."user_timeline" (
+    "id" "uuid" DEFAULT "gen_random_uuid"() NOT NULL,
+    "created_at" timestamp with time zone DEFAULT ("now"() AT TIME ZONE 'utc'::"text") NOT NULL,
+    "user_id" "uuid" NOT NULL,
+    "score" integer NOT NULL,
+    "is_read" boolean DEFAULT false NOT NULL,
+    "data" "jsonb" NOT NULL,
+    "repo_id" "uuid",
+    "type" "text" NOT NULL,
+    "categories" "text"[],
+    "snooze_to" timestamp with time zone,
+    "dedupe_hash" "text" NOT NULL,
+    "updated_at" timestamp with time zone DEFAULT ("now"() AT TIME ZONE 'utc'::"text") NOT NULL,
+    "event_ids" "text"[]
+);
+
+
+ALTER TABLE "public"."user_timeline" OWNER TO "postgres";
+
+
+COMMENT ON COLUMN "public"."user_timeline"."event_ids" IS 'Mostly for backtracking and debugging purposes';
+
+
+
+CREATE TABLE IF NOT EXISTS "public"."users" (
+    "id" "uuid" NOT NULL,
+    "created_at" timestamp with time zone DEFAULT ("now"() AT TIME ZONE 'utc'::"text") NOT NULL,
+    "github_username" "text" NOT NULL,
+    "email" "text" NOT NULL
+);
+
+
+ALTER TABLE "public"."users" OWNER TO "postgres";
+
+
+ALTER TABLE ONLY "public"."github_event_log"
+    ADD CONSTRAINT "github_event_log_pkey" PRIMARY KEY ("id");
+
+
+
+ALTER TABLE ONLY "public"."public_timeline"
+    ADD CONSTRAINT "public_timeline_pkey" PRIMARY KEY ("id");
+
+
+
+ALTER TABLE ONLY "public"."repositories"
+    ADD CONSTRAINT "repos_pkey" PRIMARY KEY ("id");
+
+
+
+ALTER TABLE ONLY "public"."subscriptions"
+    ADD CONSTRAINT "subscriptions_pkey" PRIMARY KEY ("id");
+
+
+
+ALTER TABLE ONLY "public"."subscriptions"
+    ADD CONSTRAINT "subscriptions_user_id_repo_id_key" UNIQUE ("user_id", "repo_id");
+
+
+
+ALTER TABLE ONLY "public"."public_timeline"
+    ADD CONSTRAINT "unique_dedupe_hash" UNIQUE ("dedupe_hash");
+
+
+
+ALTER TABLE ONLY "public"."user_timeline"
+    ADD CONSTRAINT "user_timeline_pkey" PRIMARY KEY ("id");
+
+
+
+ALTER TABLE ONLY "public"."user_timeline"
+    ADD CONSTRAINT "user_timeline_user_id_dedupe_hash_unique" UNIQUE ("user_id", "dedupe_hash");
+
+
+
+ALTER TABLE ONLY "public"."users"
+    ADD CONSTRAINT "users_pkey" PRIMARY KEY ("id");
+
+
+
+ALTER TABLE ONLY "public"."public_timeline"
+    ADD CONSTRAINT "public_timeline_repo_id_fkey" FOREIGN KEY ("repo_id") REFERENCES "public"."repositories"("id") ON UPDATE CASCADE ON DELETE SET NULL;
+
+
+
+ALTER TABLE ONLY "public"."subscriptions"
+    ADD CONSTRAINT "subscriptions_repo_id_fkey" FOREIGN KEY ("repo_id") REFERENCES "public"."repositories"("id") ON UPDATE CASCADE ON DELETE CASCADE;
+
+
+
+ALTER TABLE ONLY "public"."subscriptions"
+    ADD CONSTRAINT "subscriptions_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "auth"."users"("id") ON UPDATE CASCADE ON DELETE CASCADE;
+
+
+
+ALTER TABLE ONLY "public"."user_timeline"
+    ADD CONSTRAINT "user_timeline_repo_id_fkey" FOREIGN KEY ("repo_id") REFERENCES "public"."repositories"("id") ON UPDATE CASCADE ON DELETE SET NULL;
+
+
+
+ALTER TABLE ONLY "public"."user_timeline"
+    ADD CONSTRAINT "user_timeline_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "auth"."users"("id") ON UPDATE CASCADE ON DELETE CASCADE;
+
+
+
+ALTER TABLE ONLY "public"."users"
+    ADD CONSTRAINT "users_id_fkey" FOREIGN KEY ("id") REFERENCES "auth"."users"("id") ON UPDATE CASCADE ON DELETE CASCADE;
+
+
+
+CREATE POLICY "Allow users to update their own records" ON "public"."users" FOR UPDATE TO "authenticated" USING ((( SELECT "auth"."uid"() AS "uid") = "id")) WITH CHECK ((( SELECT "auth"."uid"() AS "uid") = "id"));
+
+
+
+CREATE POLICY "Enable delete for users based on id" ON "public"."users" FOR DELETE TO "authenticated" USING ((( SELECT "auth"."uid"() AS "uid") = "id"));
+
+
+
+CREATE POLICY "Enable insert for users based on user_id" ON "public"."users" FOR INSERT TO "authenticated" WITH CHECK ((( SELECT "auth"."uid"() AS "uid") = "id"));
+
+
+
+CREATE POLICY "Enable users to view their own data only" ON "public"."users" FOR SELECT TO "authenticated" USING ((( SELECT "auth"."uid"() AS "uid") = "id"));
+
+
+
+ALTER TABLE "public"."github_event_log" ENABLE ROW LEVEL SECURITY;
+
+
+ALTER TABLE "public"."public_timeline" ENABLE ROW LEVEL SECURITY;
+
+
+ALTER TABLE "public"."repositories" ENABLE ROW LEVEL SECURITY;
+
+
+ALTER TABLE "public"."subscriptions" ENABLE ROW LEVEL SECURITY;
+
+
+ALTER TABLE "public"."user_timeline" ENABLE ROW LEVEL SECURITY;
+
+
+ALTER TABLE "public"."users" ENABLE ROW LEVEL SECURITY;
+
+
+
+
+ALTER PUBLICATION "supabase_realtime" OWNER TO "postgres";
+
+
+GRANT USAGE ON SCHEMA "public" TO "postgres";
+GRANT USAGE ON SCHEMA "public" TO "anon";
+GRANT USAGE ON SCHEMA "public" TO "authenticated";
+GRANT USAGE ON SCHEMA "public" TO "service_role";
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+GRANT ALL ON TABLE "public"."github_event_log" TO "anon";
+GRANT ALL ON TABLE "public"."github_event_log" TO "authenticated";
+GRANT ALL ON TABLE "public"."github_event_log" TO "service_role";
+
+
+
+GRANT ALL ON TABLE "public"."public_timeline" TO "anon";
+GRANT ALL ON TABLE "public"."public_timeline" TO "authenticated";
+GRANT ALL ON TABLE "public"."public_timeline" TO "service_role";
+
+
+
+GRANT ALL ON TABLE "public"."repositories" TO "anon";
+GRANT ALL ON TABLE "public"."repositories" TO "authenticated";
+GRANT ALL ON TABLE "public"."repositories" TO "service_role";
+
+
+
+GRANT ALL ON TABLE "public"."subscriptions" TO "anon";
+GRANT ALL ON TABLE "public"."subscriptions" TO "authenticated";
+GRANT ALL ON TABLE "public"."subscriptions" TO "service_role";
+
+
+
+GRANT ALL ON TABLE "public"."user_timeline" TO "anon";
+GRANT ALL ON TABLE "public"."user_timeline" TO "authenticated";
+GRANT ALL ON TABLE "public"."user_timeline" TO "service_role";
+
+
+
+GRANT ALL ON TABLE "public"."users" TO "anon";
+GRANT ALL ON TABLE "public"."users" TO "authenticated";
+GRANT ALL ON TABLE "public"."users" TO "service_role";
+
+
+
+
+
+
+
+
+
+ALTER DEFAULT PRIVILEGES FOR ROLE "postgres" IN SCHEMA "public" GRANT ALL ON SEQUENCES  TO "postgres";
+ALTER DEFAULT PRIVILEGES FOR ROLE "postgres" IN SCHEMA "public" GRANT ALL ON SEQUENCES  TO "anon";
+ALTER DEFAULT PRIVILEGES FOR ROLE "postgres" IN SCHEMA "public" GRANT ALL ON SEQUENCES  TO "authenticated";
+ALTER DEFAULT PRIVILEGES FOR ROLE "postgres" IN SCHEMA "public" GRANT ALL ON SEQUENCES  TO "service_role";
+
+
+
+
+
+
+ALTER DEFAULT PRIVILEGES FOR ROLE "postgres" IN SCHEMA "public" GRANT ALL ON FUNCTIONS  TO "postgres";
+ALTER DEFAULT PRIVILEGES FOR ROLE "postgres" IN SCHEMA "public" GRANT ALL ON FUNCTIONS  TO "anon";
+ALTER DEFAULT PRIVILEGES FOR ROLE "postgres" IN SCHEMA "public" GRANT ALL ON FUNCTIONS  TO "authenticated";
+ALTER DEFAULT PRIVILEGES FOR ROLE "postgres" IN SCHEMA "public" GRANT ALL ON FUNCTIONS  TO "service_role";
+
+
+
+
+
+
+ALTER DEFAULT PRIVILEGES FOR ROLE "postgres" IN SCHEMA "public" GRANT ALL ON TABLES  TO "postgres";
+ALTER DEFAULT PRIVILEGES FOR ROLE "postgres" IN SCHEMA "public" GRANT ALL ON TABLES  TO "anon";
+ALTER DEFAULT PRIVILEGES FOR ROLE "postgres" IN SCHEMA "public" GRANT ALL ON TABLES  TO "authenticated";
+ALTER DEFAULT PRIVILEGES FOR ROLE "postgres" IN SCHEMA "public" GRANT ALL ON TABLES  TO "service_role";
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+RESET ALL;
