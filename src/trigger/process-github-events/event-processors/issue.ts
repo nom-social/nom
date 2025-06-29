c3f6143d9d925845eaa284fa09e089bb6252b24c
Wilson Ler <lws803@gmail.com>
2025-06-29 08:56:03 -0400

    removed specific definition of issue schema

--- src/trigger/process-github-events/event-processors/issue.ts
+++ src/trigger/process-github-events/event-processors/issue.ts
@@ -1,3 +1,4 @@
+import z from "zod";
 import crypto from "crypto";
 import { Octokit } from "@octokit/rest";
 
@@ -5,8 +6,35 @@ import { Json, TablesInsert } from "@/types/supabase";
 import { createClient } from "@/utils/supabase/background";
 
 import { BASELINE_SCORE, ISSUE_MULTIPLIER } from "./shared/constants";
-import { issueSchema } from "./issues/schemas";
 import { generateIssueData } from "./issues/utils";
+
+const issueSchema = z.object({
+  action: z.enum(["opened", "closed", "reopened", "assigned", "edited"]),
+  issue: z.object({
+    number: z.number(),
+    title: z.string(),
+    body: z.string().nullable(),
+    html_url: z.string(),
+    created_at: z.coerce.date(),
+    updated_at: z.coerce.date(),
+    state: z.enum(["open", "closed"]),
+    user: z.object({ login: z.string() }),
+    author_association: z.enum([
+      "COLLABORATOR",
+      "CONTRIBUTOR",
+      "FIRST_TIMER",
+      "FIRST_TIME_CONTRIBUTOR",
+      "MANNEQUIN",
+      "MEMBER",
+      "NONE",
+      "OWNER",
+    ]),
+    assignee: z.object({ login: z.string() }).nullable(),
+    assignees: z.array(z.object({ login: z.string() })),
+    labels: z.array(z.object({ name: z.string() })),
+    comments: z.number(),
+  }),
+});
 
 export async function processIssueEvent({
   event,
