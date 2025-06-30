142a1fcef1a352e224e83fb645691a86f2897aac
Wilson Ler <lws803@gmail.com>
2025-06-29 20:53:55 -0400

    added timeline likes table

--- src/types/supabase.ts
+++ src/types/supabase.ts
@@ -183,6 +183,35 @@ export type Database = {
           }
         ];
       };
+      timeline_likes: {
+        Row: {
+          created_at: string;
+          dedupe_hash: string;
+          id: string;
+          user_id: string;
+        };
+        Insert: {
+          created_at?: string;
+          dedupe_hash: string;
+          id?: string;
+          user_id: string;
+        };
+        Update: {
+          created_at?: string;
+          dedupe_hash?: string;
+          id?: string;
+          user_id?: string;
+        };
+        Relationships: [
+          {
+            foreignKeyName: "likes_user_id_fkey";
+            columns: ["user_id"];
+            isOneToOne: false;
+            referencedRelation: "users";
+            referencedColumns: ["id"];
+          }
+        ];
+      };
       user_timeline: {
         Row: {
           categories: string[] | null;
