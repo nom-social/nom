8edf0f7e5b5780061a7aa86d38c34862e7fcaad9
Wilson Ler <lws803@gmail.com>
2025-06-29 20:28:04 -0400

    reverted styling

--- src/components/ui/sonner.tsx
+++ src/components/ui/sonner.tsx
@@ -7,28 +7,26 @@ const Toaster = ({ ...props }: ToasterProps) => {
   const { theme = "system" } = useTheme();
 
   return (
-    <>
-      <Sonner
-        theme={theme as ToasterProps["theme"]}
-        className="toaster group"
-        style={
-          {
-            "--normal-bg": "var(--popover)",
-            "--normal-text": "var(--popover-foreground)",
-            "--normal-border": "var(--border)",
-          } as React.CSSProperties
-        }
-        toastOptions={{
-          classNames: {
-            success:
-              "!bg-[var(--nom-green)] !text-black !border-none !shadow-none",
-            error:
-              "!bg-[var(--color-destructive)] !text-white !border-none !shadow-none",
-          },
-        }}
-        {...props}
-      />
-    </>
+    <Sonner
+      theme={theme as ToasterProps["theme"]}
+      className="toaster group"
+      style={
+        {
+          "--normal-bg": "var(--popover)",
+          "--normal-text": "var(--popover-foreground)",
+          "--normal-border": "var(--border)",
+        } as React.CSSProperties
+      }
+      toastOptions={{
+        classNames: {
+          success:
+            "!bg-[var(--nom-green)] !text-black !border-none !shadow-none",
+          error:
+            "!bg-[var(--color-destructive)] !text-white !border-none !shadow-none",
+        },
+      }}
+      {...props}
+    />
   );
 };
 
