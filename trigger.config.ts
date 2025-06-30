9e95d68d72587807bca97fba4be28573b2b1564e
Wilson Ler <lws803@gmail.com>
2025-06-30 17:25:30 -0400

    Added Sentry for error monitoring (#82)
    * added sentry
    
    * Added Sentry to trigger dev
    
    * added sentry dsn as an env var
    
    * removed unused task
    
    * simplified args

--- trigger.config.ts
+++ trigger.config.ts
@@ -1,4 +1,7 @@
 import { defineConfig } from "@trigger.dev/sdk/v3";
+import { esbuildPlugin } from "@trigger.dev/build/extensions";
+import { sentryEsbuildPlugin } from "@sentry/esbuild-plugin";
+import * as Sentry from "@sentry/node";
 
 export default defineConfig({
   project: "proj_dbbjgfoapapbbywhkgjq",
@@ -19,4 +22,32 @@ export default defineConfig({
     },
   },
   dirs: ["./src/trigger"],
+  build: {
+    extensions: [
+      esbuildPlugin(
+        sentryEsbuildPlugin({
+          org: "nom-zu",
+          project: "nom",
+          authToken: process.env.SENTRY_AUTH_TOKEN,
+        }),
+        { placement: "last", target: "deploy" }
+      ),
+    ],
+  },
+  init: async () => {
+    Sentry.init({
+      defaultIntegrations: false,
+      dsn: process.env.SENTRY_DSN,
+      environment:
+        process.env.NODE_ENV === "production" ? "production" : "development",
+    });
+  },
+  onFailure: async (payload, error, { ctx }) => {
+    Sentry.captureException(error, {
+      extra: {
+        payload,
+        ctx,
+      },
+    });
+  },
 });
