68ca9b1368099f8beee29de3bb0a92698216ac31
Wilson Ler <lws803@gmail.com>
2025-07-02 21:59:09 -0400

    added supabase anon key as fallback

--- src/utils/supabase/background.ts
+++ src/utils/supabase/background.ts
@@ -4,5 +4,6 @@ import { Database } from "@/types/supabase";
 export const createClient = () =>
   createSupabaseClient<Database>(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.SUPABASE_SERVICE_ROLE_KEY!
+    process.env.SUPABASE_SERVICE_ROLE_KEY! ||
+      process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
   );
--- src/utils/supabase/server.ts
+++ src/utils/supabase/server.ts
@@ -6,7 +6,8 @@ import { Database } from "@/types/supabase";
 export const createClient = (cookieStore: ReturnType<typeof cookies>) => {
   return createServerClient<Database>(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.SUPABASE_SERVICE_ROLE_KEY!,
+    process.env.SUPABASE_SERVICE_ROLE_KEY! ||
+      process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
     {
       cookies: {
         async getAll() {
