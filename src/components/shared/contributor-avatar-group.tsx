32e714986c2de71992cdf82fb07b5856a97ebda5
Wilson Ler <lws803@gmail.com>
2025-07-04 13:08:53 -0400

    removed deduplication

--- src/components/shared/contributor-avatar-group.tsx
+++ src/components/shared/contributor-avatar-group.tsx
@@ -19,7 +19,7 @@ export default function ContributorAvatarGroup({
 }) {
   return (
     <div className={`flex -space-x-2 ${className}`}>
-      {Array.from(new Set(contributors)).map((contributor, idx) => (
+      {contributors.map((contributor, idx) => (
         <div className="relative group" key={contributor.name + idx}>
           <a
             href={`https://github.com/${contributor.name}`}
