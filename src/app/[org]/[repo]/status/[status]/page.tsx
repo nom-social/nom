e2b9bd6cfd727d0afdc72b5a2a4f51b378af0411
Wilson Ler <lws803@gmail.com>
2025-07-07 00:00:26 -0400

    refactoring

--- src/app/[org]/[repo]/status/[status]/page.tsx
+++ src/app/[org]/[repo]/status/[status]/page.tsx
@@ -47,11 +47,7 @@ export default async function StatusPage({
           <p className="text-foreground text-lg uppercase break-all">{repo}</p>
         </Button>
       </Link>
-      <ActivityCard 
-        item={statusItem} 
-        repo={repo} 
-        org={org} 
-      />
+      <ActivityCard item={statusItem} repo={repo} org={org} />
     </main>
   );
 }
