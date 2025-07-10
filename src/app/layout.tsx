54f718a80ac967cf9f51d39f2db7beb05ce75ce2
Wilson Ler <lws803@gmail.com>
2025-07-10 07:58:19 -0400

    higher z-index for nav bar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -44,7 +44,7 @@ export default function RootLayout({
         <ThemeProvider attribute="class" defaultTheme="dark">
           <ReactQueryProvider>
             <Toaster />
-            <NavigationMenu className="w-full min-w-full bg-background fixed top-0 left-0 z-50 border-b border-border shadow-sm p-2">
+            <NavigationMenu className="w-full min-w-full bg-background fixed top-0 left-0 z-60 border-b border-border shadow-sm p-2">
               <div className="max-w-3xl mx-auto w-full">
                 <NavigationMenuList className="justify-between items-center">
                   <div className="flex flex-row gap-2 items-center">
