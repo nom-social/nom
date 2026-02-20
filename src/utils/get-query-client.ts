9821480caa5f8514e1dd31f5b0d0a167c5589f30
Wilson Ler <lws803@gmail.com>
2026-02-20 12:16:50 -0500

    use react cache instead

--- src/utils/get-query-client.ts
+++ src/utils/get-query-client.ts
@@ -1,11 +1,13 @@
 import { QueryClient } from "@tanstack/react-query";
+import { cache } from "react";
 
-export function getQueryClient() {
-  return new QueryClient({
-    defaultOptions: {
-      queries: {
-        staleTime: 60 * 1000,
+export const getQueryClient = cache(
+  () =>
+    new QueryClient({
+      defaultOptions: {
+        queries: {
+          staleTime: 60 * 1000,
+        },
       },
-    },
-  });
-}
+    })
+);
