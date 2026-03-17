5e28e0c503b95056365150e8f53d53e2b112a947
Wilson Ler <lws803@gmail.com>
2026-03-17 08:42:11 -0400

    replace back button with back to feed

--- src/app/@modal/(...)[org]/[repo]/status/[status]/page/status-modal.tsx
+++ src/app/@modal/(...)[org]/[repo]/status/[status]/page/status-modal.tsx
@@ -1,8 +1,15 @@
 "use client";
 
 import { useRouter } from "next/navigation";
+import { ArrowLeftIcon } from "lucide-react";
 
-import { Dialog, DialogContent, DialogTitle } from "@/components/ui/dialog";
+import {
+  Dialog,
+  DialogClose,
+  DialogContent,
+  DialogTitle,
+} from "@/components/ui/dialog";
+import { Button } from "@/components/ui/button";
 import * as VisuallyHidden from "@radix-ui/react-visually-hidden";
 
 import StatusActivityCard from "@/app/[org]/[repo]/status/[status]/page/status-activity-card";
@@ -24,11 +31,25 @@ export function StatusModal({ item, org, repo }: StatusModalProps) {
         if (!open) router.back();
       }}
     >
-      <DialogContent className="border-0 p-0 overflow-y-auto rounded-none top-0 left-0 translate-x-0 translate-y-0 w-full max-w-full h-full max-h-screen sm:rounded-lg sm:top-[50%] sm:left-[50%] sm:translate-x-[-50%] sm:translate-y-[-50%] sm:max-w-3xl sm:w-full sm:h-auto sm:max-h-[90vh]">
+      <DialogContent
+        showCloseButton={false}
+        className="border-0 p-0 overflow-y-auto rounded-none top-0 left-0 translate-x-0 translate-y-0 w-full max-w-full h-full max-h-screen sm:rounded-lg sm:top-[50%] sm:left-[50%] sm:translate-x-[-50%] sm:translate-y-[-50%] sm:max-w-3xl sm:w-full sm:h-auto sm:max-h-[90vh]"
+      >
         <VisuallyHidden.Root>
           <DialogTitle>{item.id}</DialogTitle>
         </VisuallyHidden.Root>
-        <StatusActivityCard item={item} repo={repo} org={org} />
+        <div className="flex flex-col gap-4 px-2 pt-2">
+          <DialogClose asChild>
+            <Button
+              variant="ghost"
+              className="flex flex-row gap-3 items-center w-full justify-start py-2 h-fit"
+            >
+              <ArrowLeftIcon />
+              <p className="text-foreground text-lg">Feed</p>
+            </Button>
+          </DialogClose>
+          <StatusActivityCard item={item} repo={repo} org={org} />
+        </div>
       </DialogContent>
     </Dialog>
   );
